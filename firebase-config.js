1acf95b32d743e7aa983539660b20f1572329bc6
dow42 <sonowalkshitis@gmail.com>
2025-05-27 18:27:13 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgCrl78GwcFLpJ6pRDAvrVYkXkgcLHSkI",
+  authDomain: "assamchat-29eef.firebaseapp.com",
+  projectId: "assamchat-29eef",
+  storageBucket: "assamchat-29eef.appspot.com",
+  messagingSenderId: "686012350709",
+  appId: "1:686012350709:web:1ea7f5f0444a1a402a85bc",
+  measurementId: "G-GDPW28JVTF"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
